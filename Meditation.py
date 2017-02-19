719bfbfca270baed42e8373805e2c3ccbe0dac4f
sstipe <sstipe@stipeco.com>
2017-02-18 22:49:34 -0600

    Update Meditation.py

--- Meditation.py
+++ Meditation.py
@@ -1,10 +1,17 @@
+####################
+##Meditation##
+#######################################################################
+##This Limits OR "focuses" the output of MindwaveMobile parser to the##
+##MeditationDataPoint(dataPoint)########################################
+#################################
+#First, we grab our input from the MindwaveMobile
 import time
 import bluetooth
 from MindwaveDataPoints import RawDataPoint
 from MindwaveDataPointReader import MindwaveDataPointReader
 ##Added following to stock files from Neurosky
 from MindwaveDataPoints import MeditationDataPoint
-##
+##Now we need to display the Meditation Value each time it is next in the queue
 
 if __name__ == '__main__':
     mindwaveDataPointReader = MindwaveDataPointReader()
